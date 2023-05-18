f9713d4eb3dc9b2969ad018cc3a1c464109e718f
aseallotfe <133778455+aseallotfe@users.noreply.github.com>
2023-05-18 15:23:39 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+Console.WriteLine("enter first number(x)");
+double r = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter second number(y)");
+double l = Convert.ToDouble(Console.ReadLine());
+point re = new point(r, l);
+re.move(r,l);
+re.Quadrant(r, l);
+re.print(r,l);
---
+++ ConsoleApp1/point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class point
+    {
+        private double x;
+        private double y;
+        public point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+        public void move(double x, double y)
+        {
+            x = x - 5;
+            y = y + 5;
+            Console.WriteLine("x=" + x);
+            Console.WriteLine("y=" + y);
+        }
+        public void Quadrant(double x,double y)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("first quarter");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("second quarter");
+            }
+            else if(x<0&&y<0)
+            {
+                Console.WriteLine("third quarter");
+            }
+            else
+            {
+                Console.WriteLine("four quarter");
+            }
+        }
+        public void print(double x,double y)
+        {
+            Console.WriteLine("(" + x + "," + y + ")");
+        }
+
+    }
+}
